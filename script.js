6f7cef5cfe47929b987f7bf7b72cda0ae21f3c45
MuntjeerAli <muntjeersayyad@gmail.com>
2022-02-19 13:41:52 +0530

    learning javascript

---
+++ script.js
@@ -0,0 +1,57 @@
+let challengeName = 'Live Tutorial of JavaScript';
+
+console.log(challengeName);
+
+console.log(challengeName.length);
+
+console.log(challengeName.toUpperCase());
+
+console.log(challengeName.toLowerCase());
+
+console.log(challengeName.substring(0, 4));
+
+console.log(challengeName.includes('Script'));
+
+console.log(challengeName.split(''));
+
+console.log(challengeName.split(' '));
+
+let companyNames = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+
+console.log(companyNames.split(','))
+
+console.log(challengeName.replace('JavaScript', 'Python'));
+
+console.log(challengeName.charAt(15));
+
+console.log(challengeName.charCodeAt('J'));
+
+console.log(challengeName.indexOf('a'));
+
+console.log(challengeName.lastIndexOf('a'));
+
+let sentenceOne = 'You cannot end a sentence with because because because is a conjunction.'
+
+console.log(sentenceOne.indexOf('because'));
+
+console.log(sentenceOne.lastIndexOf('because'));
+
+console.log(sentenceOne.search('because'));
+
+console.log(challengeName.trim(' '));
+
+console.log(challengeName.startsWith('Live'));
+
+console.log(challengeName.endsWith('JavaScript'));
+
+console.log(challengeName.match('a'));
+
+let tutorialA = 'Live Tutorial of';
+
+let script = 'JavaScript';
+
+let overall = tutorialA + ' ' + script;
+
+console.log(overall);
+
+console.log(challengeName.repeat(2));
